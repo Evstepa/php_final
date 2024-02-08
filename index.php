7a3c22e98bbfcdbec83df03b69e4a9032259b37c
Елена Степаненко <stepanenko.ei@yandex.ru>
2024-02-08 20:12:53 +0300

    composer init

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+// require_once 'vendor/autoload.php';
+
 const URL = [
-  "users/list" => "",
-  "users/get/{id}" => "",
-  "users/update" => "",
-  "users/login" => "",
-  "users/logout" => "",
-  "users/reset_password" => "",
+    "users/list" => "",
+    "users/get/{id}" => "",
+    "users/update" => "",
+    "users/login" => "",
+    "users/logout" => "",
+    "users/reset_password" => "",
 ];
