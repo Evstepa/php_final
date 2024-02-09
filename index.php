ef0211bc780b1f17a8aaa9bd1a5642311cb63242
Елена Степаненко <stepanenko.ei@yandex.ru>
2024-02-09 22:54:58 +0300

    Методы find

--- index.php
+++ index.php
@@ -21,34 +21,32 @@ var_dump($db->findAll());
 
 echo "<br>--------<br>";
 
-var_dump($db->getConnection());
+// var_dump($db->getConnection());
 
-echo "<br>--------<br>";
+// echo "<br>--------<br>";
 
-$currentConnect = $db->getConnection();
+// $currentConnect = $db->getConnection();
 
-$user = [
-    'email' => 'User1@cloud.ru',
-    'password' => '111111',
-    'name' => 'User1',
-    'surname' => 'Uuser1',
-    'age' => 20,
-];
+// $user = [
+//     'email' => 'User3@cloud.ru',
+//     'password' => '111111',
+//     'name' => 'User3',
+//     'surname' => 'Uuser3',
+//     'age' => 33,
+//     'folder' => sprintf("%s-%s-%d", 'User3', 'Uuser3', rand(1, PHP_INT_MAX)),
+//     'createdAt' => (new \DateTime())->format('Y-m-d H:i:s'),
+//     'updatedAt' => (new \DateTime())->format('Y-m-d H:i:s'),
+// ];
 
-$state = $currentConnect->prepare("INSERT INTO user(id, email, password, name, surname, age, folder, createdAt, updatedAt)
-values(null, :email, :password, :name, :surname, :age, :folder, :createdAt, :updatedAt)");
+// $state = $currentConnect->prepare("INSERT INTO user(id, email, password, name, surname, age, folder, createdAt, updatedAt)
+// values(null, :email, :password, :name, :surname, :age, :folder, :createdAt, :updatedAt)");
 
-$state->execute([
-    'email' => $user['email'],
-    'password' => $user['password'],
-    'name' => $user['name'],
-    'surname' => $user['surname'],
-    'age' => $user['age'],
-    'folder' => sprintf("%s-%s-%d", $user['name'], $user['surname'], rand(1, PHP_INT_MAX)),
-    'createdAt' => (new \DateTime())->format('Y-m-d H:i:s'),
-    'updatedAt' => (new \DateTime())->format('Y-m-d H:i:s')
-]);
+// $state->execute($user);
 
-var_dump($db->findAll());
+// var_dump($db->find(2));
+
+// var_dump($db->findOneBy(['name' => 'User3']));
+
+// var_dump($db->findBy(['name' => 'User']));
 
 echo "<br>--------<br>";
--- src/Core/Db.php
+++ src/Core/Db.php
@@ -46,6 +46,9 @@ class Db
         return $this->connect;
     }
 
+    /**
+     * @return array
+     */
     public function findAll(): array
     {
         $state = $this->connect->prepare("SELECT * FROM user WHERE 1");
@@ -53,8 +56,39 @@ class Db
         return $state->fetchAll();
     }
 
-    // findOneBy();
-    // findBy();
-    // find().
+    /**
+     * @param int $id
+     * @return Uarray|null
+     */
+    public function find(int $id): ?array
+    {
+        $state = $this->connect->prepare("SELECT * FROM user WHERE id = :id");
+        $state->execute(['id' => $id]);
+        return $state->fetch();
+    }
 
+    /**
+     * @param array $criteria
+     * @return array|null
+     */
+    public function findOneBy(array $criteria): ?array
+    {
+        $sql = sprintf("SELECT * FROM user WHERE %s = :%s", array_keys($criteria)[0], array_keys($criteria)[0]);
+        $state = $this->connect->prepare($sql);
+        $state->execute($criteria);
+        return $state->fetch();
+    }
+
+    /**
+     * @param array $criteria
+     * @return array
+     */
+    public function findBy(array $criteria): array
+    {
+        $pattern = '%' . array_values($criteria)[0] . '%';
+        $sql = sprintf("SELECT * FROM user WHERE %s LIKE :pattern", array_keys($criteria)[0]);
+        $state = $this->connect->prepare($sql);
+        $state->execute([':pattern' => $pattern]);
+        return $state->fetchAll();
+    }
 }
